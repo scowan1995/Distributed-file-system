df9dffe1b8c62b62c42a9fb6943371deb8e0a339
scowan1995 <scowan@tcd.ie>
2016-12-02 14:57:14 +0000

    changing to a servant implementation, haskell is hard omg

--- src/server.hs
+++ src/server.hs
@@ -1,25 +1,63 @@
--- This will act as the main server for allowing clients to access their files
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
-import Network.Socket
+module Server where
 
-main = runServer
+import Prelude ()
+import Prelude.Compat
 
-runServer :: IO()
-runServer = do
-  s <- socket AF_INET Stream defaultProtocol
-  --creates a TCP socket
-  setSocketOption s ReuseAddr 1
-  bind s (SockAddrInet 3001 iNADDR_ANY)
-  listen s 5
-  handleConn s
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson.Compat
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Lucid
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import Text.Blaze
+import Text.Blaze.Html.Renderer.Utf8
+import qualified Data.Aeson.Parser
+import qualified Text.Blaze.Html
 
-handleConn :: Socket -> IO()
-handleConn s = do
-  connection <- accept s
-  handleData connection
-  putStrLn (show connection)
+type userAPI' = "users" :> GET '[JSON] [User]
 
-handleData :: (Socket, SockAddr) -> IO()
-handleData (s, _) = do
-  send s "You have connected to the server, well done!"
-  close s
+data User = User {
+  username :: String,
+  password :: String
+} deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users :: [User]
+users = 
+  [ User "Admin" "password",
+    User "John" "john",
+    User "Mary" "mary"
+  ] 
+
+server' :: Server userAPI'
+server' = return users
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+app :: Application
+app = serve userAPI server'
+
+main :: IO ()
+main = run 2000 app
